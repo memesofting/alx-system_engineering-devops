99006d42055e8774d7f84749cd1a4e1c84d211e4
memesofting <bibitayoadekunle00@gmail.com>
2024-08-13 23:51:06 +0100

    get number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    (total subscribers) for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "subreddit-subscriber-counter/0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
